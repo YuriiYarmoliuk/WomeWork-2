2f5a3be560a04ed60e0ecb5679a4214f3b6d3438
YuriiYarmoliuk <116096922+YuriiYarmoliuk@users.noreply.github.com>
2022-11-12 13:32:31 +0200

    Update AverageNumbers.java

--- AverageNumbers.java
+++ AverageNumbers.java
@@ -10,10 +10,8 @@ public class AverageNumbers {
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
         }
-        double avarage = 0;
-        for (int i = 0; i < nums.length; i++) {
-            avarage = (double) sum / nums.length;
-        }
+        double avarage = (double) sum / nums.length
+            
         System.out.println("Avarage of a list of numbers = " + avarage);
     }
 }
