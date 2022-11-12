667a039640a082270d9a8945a0ef94eff8628339
YuriiYarmoliuk <116096922+YuriiYarmoliuk@users.noreply.github.com>
2022-11-12 13:49:09 +0200

    Update ArrayRevers.java

--- ArrayRevers.java
+++ ArrayRevers.java
@@ -1,19 +1,15 @@
 package HW2;
 
+import java.util.Collections;
 import java.util.Arrays;
 
 public class ArrayRevers {
+        public static void main(String[] args) {
+            Integer[] nums = {2, 3, 1, 7, 11};
+            Arrays.sort(nums, Collections.reverseOrder());
 
-    public static void main(String[] args) {
-
-        int[] nums = {2, 3, 1, 7, 11};
-        sortAndPrint(nums);
+            for (int values : nums) {
+                System.out.print(values + " ");
+            }
+        }
     }
-    public static void sortAndPrint(int[] m) {
-
-        Arrays.sort(m);
-        for (int i = m.length - 1; i >= 0; i--)
-            System.out.print(m[i] + " ");
-        System.out.println("");
-    }
-}
